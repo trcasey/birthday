d7263726dc5703f596ac2fe5b5b709baaaa713fa
Trent Casey <trcasey@outlook.com>
2016-03-21 21:19:31 -0400

    finished adding the questions.

--- program.py
+++ program.py
@@ -1,12 +1,23 @@
+import datetime
+
 def print_header():
-    pass
+    print('---------------------------------------')
+    print('             BIRTHDAY APP              ')
+    print('---------------------------------------')
+    print()
 
 
 def get_birthday_from_user():
-    pass
+    print('Tell us when you were born: ')
+    year = int(input('Year [YYYY]: '))
+    month = int(input('Month [MM]: '))
+    day = int(input('Day [DD]: '))
+
+    birthday = datetime.datetime(year, month, day)
+    return birthday
 
 
-def compute_days_between_dates():
+def compute_days_between_dates(date1, date2):
     pass
 
 
